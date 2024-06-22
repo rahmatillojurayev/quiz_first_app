ae4e88e5388f1be6c40ac4b67a13c32d3bc6b0ec
rahmatillojuraev <raxmatillojurayev94@gmail.com>
2024-06-22 14:10:05 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/quiz_first_app/QuizFirstAppApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.quiz_first_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizFirstAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/pdp/quiz_first_app/QuizFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.quiz_first_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
